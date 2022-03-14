38a281af8c79f8a5de26c4f2107fdc9fa6036434
JongHoonSon <roka1684@gmail.com>
2022-03-15 00:30:24 +0900

    Setup router, controller and pug

---
+++ src/controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send("join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (Zreq, res) => res.send("Remove User");
+export const login = (Zreq, res) => res.send("Login");
+export const logout = (Zreq, res) => res.send("Log Out");
+export const see = (Zreq, res) => res.send("See");
+
+export default join;
