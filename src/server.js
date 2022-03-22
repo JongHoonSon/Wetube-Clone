887cf81e658aac9b0d4d79c319f4c3f704daeeb9
JongHoonSon <roka1684@gmail.com>
2022-03-23 02:47:40 +0900

    #7.10 Logged In User part Two
    localMiddleware 사용 (res.locals 에 Pug에서 사용되는 다양한 값을 저장)

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -21,20 +22,8 @@ app.use(
         saveUninitialized: true,
     })
 );
-
-app.get("/add-one", (req, res, next) => {
-    req.session.potato += 1;
-    return res.send(`${req.session.id}\n${req.session.potato}`);
-});
-
-app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-      console.log(sessions);
-      next();
-    });
-});
   
-
+app.use(localMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
