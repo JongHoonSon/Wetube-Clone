2a1320b5c44e26fc3f2894024b248cc99a021ab5
JongHoonSon <roka1684@gmail.com>
2022-03-22 23:46:45 +0900

    #7.0 Create Account part One
    Join 페이지 구성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,10 @@
-export const join = (req, res) => res.send("join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+    console.log(req.body);
+    return res.end();
+}
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (Zreq, res) => res.send("Remove User");
 export const login = (Zreq, res) => res.send("Login");
 export const logout = (Zreq, res) => res.send("Log Out");
 export const see = (Zreq, res) => res.send("See");
-
-export default join;
