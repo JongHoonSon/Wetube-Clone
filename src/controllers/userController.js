82e5126754816388ba4adb6ef7abb96ad9cd9a9a
JongHoonSon <roka1684@gmail.com>
2022-03-24 03:18:15 +0900

    #8.6 File Uploads part One
    postEdit 컨트롤러에서 req.file 로 주어지는 업로드된 사진 확인

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -139,7 +139,9 @@ export const postEdit = async (req, res) => {
             user: { _id }
         },
         body: { name, email, username, location },
+        file,
     } = req;
+    console.log(file);
     const exists = await User.exists({$or: [{username}, {email}]});
     if(exists) {
         return res.status(400).render("edit-profile", { pageTitle: "Edit profile", errorMessage: "This username/email is already taken."});
