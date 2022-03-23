a0328ce0f09138280f76e9ff088656249da5ffa6
JongHoonSon <roka1684@gmail.com>
2022-03-23 22:00:33 +0900

    console.log(res.locals) 삭제

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,6 @@ export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 true 일 경우 res.locals.loggedIn 을 true로 함.
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user;
-    console.log(res.locals);
+    // console.log(res.locals);
     next();
 }
