f87abab2622bd7df5ecced70cab3611c6b2c8dca
JongHoonSon <roka1684@gmail.com>
2022-03-25 01:45:43 +0900

    #8.9 Video Upload
    Video 스키마에 file.path 추가

--- src/Models/Video.js
+++ src/Models/Video.js
@@ -4,6 +4,7 @@ const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, maxlength: 80 },
     description: { type: String, required: true, minlength: 20 },
     createdAt: { type: Date, required: true, default: Date.now},
+    fileUrl: { type:String, required: true },
     hashtags: [{ type: String, trim: true }],
     meta: {
         views: { type: Number, required: true,  default: 0, trim: true },
