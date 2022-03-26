3ba1c7488435fad5c78da0b93114df3f6fdd6f1c
JongHoonSon <roka1684@gmail.com>
2022-03-27 00:24:59 +0900

    #11.3 Volume
    볼륨 버튼 관련 JS 작성

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -6,7 +6,11 @@ const volumeRange = document.getElementById("volume");
 
 // console.log(video, play, mute, time, volume);
 
-const handlePlayClick = (e) => {
+let volumeValue = 0.5;
+
+video.volume = volumeValue;
+
+const handlePlayClick = (event) => {
     if(video.paused) {
         video.play();
     } else {
@@ -15,15 +19,26 @@ const handlePlayClick = (e) => {
     playBtn.innerText = video.paused ? "Play" : "Pause";
 }
 
-const handleMute = (e) => {
+const handleMute = (event) => {
     if(video.muted) {
         video.muted = false;
     } else {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute"
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
+}
+
+const handleVolumeChange = (event) => {
+    const { target: { value } } = event;
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
 }
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
