f7e463345b18a63d23d206325e8d823c95917734
JongHoonSon <roka1684@gmail.com>
2022-03-27 00:42:35 +0900

    #11.4 Duration and Current Time
    video 현재 재생 중인 시간, 총 시간 표시

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 // console.log(video, play, mute, time, volume);
 
@@ -39,6 +41,16 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 }
 
+const handleLoadedMetaData = (event) => {
+    totalTime.innerText = Math.floor(video.duration);  // loadmetadata 를 다루는 핸들러 안에서만 사용 가능함.
+}
+
+const handleTimeUpdate = (event) => {
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData)
+video.addEventListener("timeupdate", handleTimeUpdate)
