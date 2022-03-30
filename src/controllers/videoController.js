2aa977789186cdff55f66c9deb32c3c4a15ed741
JongHoonSon <roka1684@gmail.com>
2022-03-31 01:57:46 +0900

    #16.3 API Route part One
    API가 호출되는 URL에 router 및 controller 설정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,6 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id).populate("owner");
-    console.log(video);
     if(!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
@@ -125,3 +124,9 @@ export const registerView = async (req, res) => {
     await video.save();
     return res.sendStatus(200);
 }
+
+export const createComment = (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    return res.end();
+  };
