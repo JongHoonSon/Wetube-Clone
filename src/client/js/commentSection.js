fd95a1b037a457ec37bc7588082f426a93a051e3
JongHoonSon <roka1684@gmail.com>
2022-03-31 02:23:48 +0900

    #16.4 API Route part Two
    fetch로 데이터를 전송할 때는 body에 보낼 객체를 JSON.stringify로 json 형태로 바꾸고,
    headers 에 이것은 json 형태로 된 String임을 알리는 정보를 함께 보낸다

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -6,11 +6,16 @@ const handleSubmit = (event) => {
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
+  
+  if (text === "") {
+    return ;
+  }
   fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
-    body: {
-      text,
-    },
+    headers: {
+      "Content-Type": "application/json", // express 에게 POST에 담긴 내용이 JSON 형태의 String 임을 headers에서 전달함
+    },                                    // express야 이거 바꿔줘 ~ 근데 내가 미리 JSON 형태로 바꿔놨어~
+    body: JSON.stringify({ text }),
   });
 };
 
