f9a44e0c1d34bdb8cb846c61722fdfbaa0451774
JongHoonSon <roka1684@gmail.com>
2022-03-30 23:53:48 +0900

    #14.6 Thumbnail Upload part Two
    모든 동영상이 5초가 되도록 setTimeout 설정, handleStop 삭제

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -72,18 +72,10 @@ const handleDownload = async () => {
     actionBtn.addEventListener("click", handleStart);
 }
 
-const handleStop = () => {
-    actionBtn.innerText = "Download recorded video";
-    actionBtn.removeEventListener("click", handleStop);
-    actionBtn.addEventListener("click", handleDownload);
-
-    recorder.stop();
-}
-
 const handleStart = () => {
-    actionBtn.innerText = "Stop Recording";
+    actionBtn.innerText = "Recording";
+    actionBtn.disabled = true;
     actionBtn.removeEventListener("click", handleStart);
-    actionBtn.addEventListener("click", handleStop);
 
     recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (e) => {
@@ -92,8 +84,14 @@ const handleStart = () => {
         video.src = videoFile;
         video.loop = true;
         video.play();
-    }
+        actionBtn.innerText = "Download";
+        actionBtn.disabled = false;
+        actionBtn.addEventListener("click", handleDownload);
+    };
     recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 5000);
 }
 
 const init = async () => {
