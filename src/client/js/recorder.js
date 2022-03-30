93e2ea4860bcd1b5bfe40f26f2cd35f7504472b2
JongHoonSon <roka1684@gmail.com>
2022-03-30 22:38:18 +0900

    #14.3 Thumbnail
    같은 방식으로 ffmpeg 가상 세계에서 webm를 thumbnail.jpg 파일 생성 및 다운로드

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -15,22 +15,37 @@ const handleDownload = async () => {
 
     ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
 
+    // webm -> mp4변환
     await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // -i : encording, -r : frame 설정
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    // webm 에서 thumbnail 찍음
+    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");  // "-ss", "00:00:01" -> 영상의 00:00:01 시간에
+                                                                                    // "-frames:v", "1" -> 1장의 사진을 찍음
+    console.log();
 
-    console.log(mp4File);   // 실제 파일, 수정만 가능
-    console.log(mp4File.buffer); // 실제 파일, raw data(binary data)이용 가능, 버퍼를 사용해야함
+    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+    // console.log(mp4File);   // 실제 파일, 수정만 가능
+    // console.log(mp4File.buffer); // 실제 파일, raw data(binary data)이용 가능, 버퍼를 사용해야함
 
     const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+    const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
     const mp4Url = URL.createObjectURL(mp4Blob);
+    const thumbUrl = URL.createObjectURL(thumbBlob);
 
     const a = document.createElement("a");
     a.href = mp4Url;
     a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
+
+    const thumbA = document.createElement("a");
+    thumbA.href = thumbUrl;
+    thumbA.download = "MyThumbnail.jpg";
+    document.body.appendChild(thumbA);
+    thumbA.click();
 }
 
 const handleStop = () => {
