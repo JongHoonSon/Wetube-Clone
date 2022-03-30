d825e95516becdde06582f54288b39909e2bd4f1
JongHoonSon <roka1684@gmail.com>
2022-03-30 22:57:43 +0900

    #14.5 Thumbnail Upload part One
    파일 이름을 객체화하여 사용(오타 예방),
    a태그를 만들어주는 함수 사용

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -6,6 +6,20 @@ let stream;
 let recorder;
 let videoFile;
 
+const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg"
+}
+
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+}
+
 const handleDownload = async () => {
 
     const ffmpeg = createFFmpeg({
@@ -13,18 +27,18 @@ const handleDownload = async () => {
     });
     await ffmpeg.load(); // 유저 사이드에서 ffmpeg가 load될 때까지 기다림
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
 
     // webm -> mp4변환
-    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // -i : encording, -r : frame 설정
+    await ffmpeg.run("-i", files.input, "-r", "60", files.output); // -i : encording, -r : frame 설정
 
     // webm 에서 thumbnail 찍음
-    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");  // "-ss", "00:00:01" -> 영상의 00:00:01 시간에
+    await ffmpeg.run("-i", files.input, "-ss", "00:00:01", "-frames:v", "1", files.thumb);  // "-ss", "00:00:01" -> 영상의 00:00:01 시간에
                                                                                     // "-frames:v", "1" -> 1장의 사진을 찍음
     console.log();
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
-    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+    const mp4File = ffmpeg.FS("readFile", files.output);
+    const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
     // console.log(mp4File);   // 실제 파일, 수정만 가능
     // console.log(mp4File.buffer); // 실제 파일, raw data(binary data)이용 가능, 버퍼를 사용해야함
@@ -36,24 +50,15 @@ const handleDownload = async () => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
-    const a = document.createElement("a");
-    a.href = mp4Url;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
-
-    const thumbA = document.createElement("a");
-    thumbA.href = thumbUrl;
-    thumbA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
-
+    downloadFile(mp4Url, "MyRecording.mp4");
+    downloadFile(thumbUrl, "MyThumbnail.jpg");
+    
     // 작업이 끝났으므로 해당 파일, URL을 삭제하여 브라우저가 빨리 동작할 수 있도록 함.
-    // 메모리 관리 차원
+    // 메모리 관>리 차원
 
-    ffmpeg.FS("unlink", "recording.webm");
-    ffmpeg.FS("unlink", "output.mp4");
-    ffmpeg.FS("unlink", "thumbnail.mp4");
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", files.output);
+    ffmpeg.FS("unlink", files.thumb);
 
     URL.revokeObjectURL(videoFile);
     URL.revokeObjectURL(mp4Url);
