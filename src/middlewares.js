73c58e592b0886883f842125cfaad051e1df5d04
JongHoonSon <roka1684@gmail.com>
2022-04-02 06:48:58 +0900

    #17.9 Production Environment
    res.locals 에 isHeroku 변수 추가, 현재 서버를 구동 중인 곳이 local인지 Heroku인지 체크하여
    업로드할 때의 경로와 이미 업로된 파일을 불러들이는 경로를 달리 한다.

--- src/middlewares.js
+++ src/middlewares.js
@@ -36,7 +36,7 @@ export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 true 일 경우 res.locals.loggedIn 을 true로 함.
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
-    res.locals.isHeroku = isHeroku || {};
+    res.locals.isHeroku = isHeroku;
     // console.log(res.locals);
     next();
 }
