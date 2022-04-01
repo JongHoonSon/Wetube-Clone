cf195da1cb4494076ff32ec48cc32f281889c559
JongHoonSon <roka1684@gmail.com>
2022-04-02 01:41:12 +0900

    #17.7 AWS S3 part Two
    S3 객체 생성 및 multer s3 객체 생성
    multer 객체의 storage 속성에 multer s3 사용

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,19 @@
+import aws from "aws-sdk";
 import multer from "multer";
+import multerS3 from "multer-s3";
+
+// API KEY 를 이용해 S3 객체 생성
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET,
+    }
+})
+
+const multerUploader = multerS3({
+    s3: s3,
+    bucket: 'jh-wetube'
+});
 
 export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 true 일 경우 res.locals.loggedIn 을 true로 함.
@@ -30,12 +45,14 @@ export const avatarUpload = multer({
     dest: 'uploads/avatars/',
     limits: {
         fileSize: 3000000,
-    }
+    },
+    storage: multerUploader,
 });
 
 export const videoUpload = multer({
     dest: 'uploads/videos/',
     limits: {
         fileSize: 10000000000, //단위 바이트
-    }
+    },
+    storage: multerUploader,
 });
