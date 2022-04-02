592a3b7f08ab7e960cb860ed1375fbc696acb5a5
JongHoonSon <roka1684@gmail.com>
2022-04-03 06:14:34 +0900

    업로드 날짜 계산이 첫번째 video만 되는 오류 수정 (querySelectorAll 로 모든 태그를 가져옴)

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,36 +1,34 @@
 import "../scss/styles.scss"
 import regeneratorRuntime from "regenerator-runtime";
 
-const uploadDate = document.getElementById("uploadDate");
+const uploadDateList = document.querySelectorAll("#uploadDate");
 
-const createDate = uploadDate.dataset.id;
-const nowDate = new Date().toISOString();
+uploadDateList.forEach(uploadDate => {
+    const createDate = uploadDate.dataset.id;
+    const nowDate = new Date().toISOString();
 
-const createYear = Number(createDate.slice(0, 4));
-const nowYear = Number(nowDate.slice(0, 4));
+    const createYear = Number(createDate.slice(0, 4));
+    const nowYear = Number(nowDate.slice(0, 4));
 
-const createMonth = Number(createDate.slice(5,7));
-const nowMonth = Number(nowDate.slice(5,7));
+    const createMonth = Number(createDate.slice(5,7));
+    const nowMonth = Number(nowDate.slice(5,7));
 
-const createDay = Number(createDate.slice(8,10));
-const nowDay = Number(nowDate.slice(8,10));
+    const createDay = Number(createDate.slice(8,10));
+    const nowDay = Number(nowDate.slice(8,10));
 
-let text = '';
+    let text = '';
 
-if(nowYear > createYear) {
-    text = nowYear - createYear + "년 전";
-} else if(nowMonth > createMonth) {
-    console.log("nowMonth", nowMonth);
-    console.log("createMonth", createMonth);
-    text = nowMonth - createMonth + "달 전";
-} else {
-    console.log("nowDay", nowDay);
-    console.log("createDay", createDay);
-    if(nowDay - createDay === 0) {
-        text = "오늘";
+    if(nowYear > createYear) {
+        text = nowYear - createYear + "년 전";
+    } else if(nowMonth > createMonth) {
+        text = nowMonth - createMonth + "달 전";
     } else {
-        text = nowDay - createDay + "일 전";
+        if(nowDay - createDay === 0) {
+            text = "오늘";
+        } else {
+            text = nowDay - createDay + "일 전";
+        }
     }
-}
 
-uploadDate.innerText = text;
+    uploadDate.innerText = text;
+});
