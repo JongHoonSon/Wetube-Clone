dc06b5141244100079412ad76660e81da960b06a
JongHoonSon <roka1684@gmail.com>
2022-04-03 03:39:05 +0900

    유저의 기본 프로필 이미지 추가

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    avatarUrl: { type: String },
+    avatarUrl: { type: String, default: "https://jh-wetube.s3.ap-northeast-2.amazonaws.com/default/unknown.jpg" },
     socialOnly: { type: Boolean, default: false },
     username: { type: String, required: true, unique: true },
     password: { type: String },
