584e3538f9e6ff4d746eacd72b3861d74981668d
JongHoonSon <roka1684@gmail.com>
2022-04-10 21:00:18 +0900

    updateComment 컨트롤러 연결

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { registerVideoView } from "../controllers/videoController";
-import { createComment, deleteComment } from "../controllers/commentController";
+import {
+  createComment,
+  deleteComment,
+  updateComment,
+} from "../controllers/commentController";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerVideoView);
 apiRouter.post("/comments/:id([0-9a-f]{24})/create", createComment);
 apiRouter.delete("/comments/:id([0-9a-f]{24})/delete", deleteComment);
+apiRouter.post("/comments/:id([0-9a-f]{24})/update", updateComment);
 
 export default apiRouter;
