b08fe5f3a82299c66e54961528ecc64116573ba5
JongHoonSon <roka1684@gmail.com>
2022-04-11 16:30:28 +0900

    Video like 버튼 클릭 시 fetch 동작

--- src/client/js/watch.js
+++ src/client/js/watch.js
@@ -1,3 +1,4 @@
+const likebtn = document.querySelector(".likebtn");
 const deleteBtn = document.querySelector(".deleteBtn");
 
 function deleteConfirm(event) {
@@ -6,4 +7,12 @@ function deleteConfirm(event) {
   }
 }
 
+function handleLikebtnClick(event) {
+  const { id } = likebtn.dataset;
+  fetch(`/videos/${id}/like`, {
+    method: "POST",
+  });
+}
+
+likebtn.addEventListener("click", handleLikebtnClick);
 deleteBtn.addEventListener("click", deleteConfirm);
