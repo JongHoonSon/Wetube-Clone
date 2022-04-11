87840b527503a61d2a79952e305c3ac05ec870a7
JongHoonSon <roka1684@gmail.com>
2022-04-11 18:08:23 +0900

    API 호출을 위한 로직 삭제  (API 호출은 redirect 등을 할 수 없고, 화면 뒷단에서 실행되고 끝남)

--- src/client/js/watch.js
+++ src/client/js/watch.js
@@ -1,4 +1,3 @@
-const likebtn = document.querySelector(".likebtn");
 const deleteBtn = document.querySelector(".deleteBtn");
 
 function deleteConfirm(event) {
@@ -7,12 +6,4 @@ function deleteConfirm(event) {
   }
 }
 
-function handleLikebtnClick(event) {
-  const { id } = likebtn.dataset;
-  fetch(`/videos/${id}/like`, {
-    method: "POST",
-  });
-}
-
-likebtn.addEventListener("click", handleLikebtnClick);
 deleteBtn.addEventListener("click", deleteConfirm);
