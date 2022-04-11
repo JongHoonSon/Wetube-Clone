9b6398d3119311833ff35f9794f826bebc330937
JongHoonSon <roka1684@gmail.com>
2022-04-11 20:34:55 +0900

    로그인한 유저의 id가 video의 likeUsers 안에 있다면 지금 이 화면을 보고 있는 유저(로그인한 유저)는 좋아요를 이미 누른 것으로 판단하고 likeBtn에 like 클래스를 추가한다.

--- src/client/js/watch.js
+++ src/client/js/watch.js
@@ -1,9 +1,22 @@
 const deleteBtn = document.querySelector(".deleteBtn");
 
+function checkUserLikeVideo() {
+  const likebtn = document.querySelector(".likebtn");
+  const videoInfo = likebtn.dataset.video;
+  const loggedInUserId = likebtn.dataset.id;
+
+  JSON.parse(videoInfo).likeUsers.forEach((likeUserId) => {
+    if (likeUserId === loggedInUserId) {
+      likebtn.classList.add("red");
+    }
+  });
+}
+
 function deleteConfirm(event) {
   if (!confirm("Do you really want to delete this video?")) {
     event.preventDefault();
   }
 }
 
+checkUserLikeVideo();
 deleteBtn.addEventListener("click", deleteConfirm);
